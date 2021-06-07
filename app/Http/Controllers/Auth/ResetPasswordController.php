0c9996ca3960e28743f6c8b8373e30fec03acd2d
Tallal Saghir Home <tallalsaghir@gmail.com>
2021-06-07 23:28:14 +0500

    upto passwords reset link

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\PasswordResetNotification;
+use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
 use DB;
